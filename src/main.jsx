e966f726d29efe1a1a7fd2bd0661dcb8feee12c9
dev-from-cbd <veryfatwombat@gmail.com>
2023-11-02 06:30:35 +1030

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-
-// import `ChakraProvider` component
 import { ChakraProvider } from "@chakra-ui/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <ChakraProvider>ChakraProvider</ChakraProvider>
     <App />
   </React.StrictMode>
 );
