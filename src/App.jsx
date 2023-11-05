9cc430f7dffc3cbd7ac63985c5ec246441e9d41c
dev-from-cbd <veryfatwombat@gmail.com>
2023-11-05 21:47:54 +1030

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+//React JS and Chakra UI + Vite. Chakra UI is a simple, modular and accessible component library.
+
 function App() {
   return (
     <>
-      React JS and Chakra UI + Vite. Chakra UI is a simple, modular and
-      accessible component library.
+      <button>Button</button>
     </>
   );
 }
