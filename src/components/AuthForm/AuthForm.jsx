8563af43ff619ef12f7c251682c691780d81882f
dev-from-cbd <veryfatwombat@gmail.com>
2023-11-13 06:36:52 +1030

    update

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -1,9 +1,123 @@
-import React from "react";
+import {
+  Box,
+  Button,
+  Flex,
+  Image,
+  Input,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const AuthForm = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  const navigate = useNavigate();
+  const [inputs, setInputs] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleAuth = () => {
+    if (!inputs.email || !inputs.password) {
+      alert("Please fill all the fields");
+      return;
+    }
+  };
+
   return (
     <>
-      <Box>Box</Box>
+      <Box border={"1px solid gray"} borderRadius={6} padding={2}>
+        <VStack spacing={4}>
+          <Image
+            src="/fake.jpeg"
+            h={39.99}
+            cursor={"pointer"}
+            alt="Logo of Project"
+          />
+          <Input
+            placeholder="Email"
+            fontSize={15}
+            type="email"
+            value={inputs.email}
+            onChange={(e) => setInputs({ ...inputs, email: e.target.value })}
+          />
+          <Input
+            placeholder="PassWord"
+            fontSize={15}
+            type="password"
+            value={inputs.password}
+            onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
+          />
+
+          {!isLogin ? (
+            <Input
+              placeholder="Confirm PassWord"
+              fontSize={15}
+              type="password"
+              value={inputs.confirmPassword}
+              onChange={(e) =>
+                setInputs({ ...inputs, confirmPassword: e.target.value })
+              }
+            />
+          ) : null}
+
+          <Button
+            w="full"
+            colorScheme="green"
+            size={"sm"}
+            fontSize={15}
+            onClick={handleAuth}
+          >
+            {isLogin ? "Log in" : "Sign Up"}
+          </Button>
+
+          {/* OR */}
+          <Flex
+            alignItems={"center"}
+            justifyContent={"center"}
+            my={4}
+            gap={1}
+            w={"full"}
+          >
+            <Box flex={2} h={"1px"} bg={"blue.500"} />
+            <Text mx={1} color={"blue"}>
+              or
+            </Text>
+            <Box flex={2} h={"1px"} bg={"blue.500"} />
+          </Flex>
+
+          <Flex
+            alignItems={"center"}
+            justifyContent={"center"}
+            cursor={"pointer"}
+          >
+            <Image src="/threads-logo.png" h={25} alt="Google Auth" />
+            <Text mx={1} color={"blue.500"}>
+              Log in with Threads
+            </Text>
+          </Flex>
+        </VStack>
+      </Box>
+
+      {/* Switch between LogIn and SignUp */}
+      <Box border={"2px solid red"} borderRadius={"6"} padding={3}>
+        <Flex alignItems={"center"} justifyContent={"center"} w={"246px"}>
+          <Box mx={1} fontSize={15}>
+            {isLogin ? "Don't have an account?" : "Already have an account?"}
+          </Box>
+          <Box
+            mx={1}
+            fontSize={15}
+            onClick={() => setIsLogin(!isLogin)}
+            color={"blue.600"}
+            cursor={"pointer"}
+          >
+            {isLogin ? "Log in" : "Sign Up"}
+          </Box>
+        </Flex>
+      </Box>
     </>
   );
 };
--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -5,17 +5,18 @@ const AuthPage = () => {
   return (
     <Flex minH={"100vh"} justifyContent={"center"} alignItems={"center"} px={4}>
       <Container maxW={"container.md"} padding={0}>
-        {/* Left hand-side */}
         <Flex justifyContent={"center"} alignItems={"center"} gap={10}>
+          {/* Left Hand-Saide */}
           <Box display={{ base: "none", md: "block" }}>
-            <Image src="/auth.png" alt="Phone Image" />
+            <Image src="/insta.png" h="400" alt="Phone Image" />
           </Box>
+          {/* Right hand-side */}
           <VStack spacing={4} alignC={"stretch"}>
             <AuthForm />
             <Box textAlign={"Center"}>Get the App!!</Box>
             <Flex gap={5} justifyContent={"center"}>
-              <Image src="/playstore.png" h={10} alt="Play Store" />
-              <Image src="/microsoft.png" h={10} alt="Microsoft Store" />
+              <Image src="/app-store.png" h={10} alt="App Store" />
+              <Image src="/googleplay.png" h={10} alt="Google Play Store" />
             </Flex>
           </VStack>{" "}
         </Flex>
