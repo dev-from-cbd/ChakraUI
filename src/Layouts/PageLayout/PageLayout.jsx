60c11e982b279b8155c170168343e52a1e53bfbc
dev-from-cbd <veryfatwombat@gmail.com>
2023-11-15 06:31:57 +1030

    update

--- src/Layouts/PageLayout/PageLayout.jsx
+++ src/Layouts/PageLayout/PageLayout.jsx
@@ -1,14 +1,21 @@
 import { Box, Flex } from "@chakra-ui/react";
+import { useLocation } from "react-router-dom";
+import SideBar from "../../components/SideBar/SideBar";
 
-const PageLayout = (children) => {
+const PageLayout = ({ children }) => {
+  const { pathname } = useLocation();
   return (
     <Flex>
       {/* The SideBar on the left */}
-      <Box>
-        <SideBar />
-      </Box>
+      {pathname !== "/auth" ? (
+        <Box w={{ base: "70px", md: "240px" }}>
+          <SideBar />
+        </Box>
+      ) : null}
       {/* The page content on the right */}
-      <Box>{children}</Box>
+      <Box flex={1} w={{ base: "calc(100% - 70px)", md: "calc(100% - 240px)" }}>
+        {children}
+      </Box>
     </Flex>
   );
 };
--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,5 +1,106 @@
+import { Avatar, Box, Flex, Link, Tooltip } from "@chakra-ui/react";
+import { Link as RouterLink } from "react-router-dom";
+import {
+  CreatePostLogo,
+  InstagramLogo,
+  InstagramMobileLogo,
+  NotificationsLogo,
+  SearchLogo,
+} from "../../assets/constants";
+
+import { AiFillHome } from "react-icons/ai";
+
 const SideBar = () => {
-  return <div>SideBar</div>;
+  const sidebarItems = [
+    {
+      icon: <AiFillHome size={25} />,
+      text: "Home",
+      link: "/",
+    },
+    {
+      icon: <SearchLogo />,
+      text: "Search",
+    },
+    {
+      icon: <NotificationsLogo />,
+      text: "Notifications",
+    },
+    {
+      icon: <CreatePostLogo />,
+      text: "Create Post",
+    },
+    {
+      icon: <Avatar size={"sm"} name="Fat Wombat" src="/profilepic.png" />,
+      text: "Profile",
+      link: "/dev-from-cbd",
+    },
+  ];
+
+  return (
+    <Box
+      height={"100vh"}
+      borderRight={"1px solid"}
+      borderColor={"whiteAlpha.300"}
+      py={8}
+      position={"sticky"}
+      top={0}
+      left={0}
+      px={{ base: 2, md: 4 }}
+    >
+      <Flex direction={"column"} gap={10} w={"full"} height={"full"}>
+        <Link
+          to={"/"}
+          as={RouterLink}
+          pl={2}
+          display={{ base: "none", md: "block" }}
+          cursor={"pointer"}
+        >
+          <InstagramLogo />
+        </Link>
+        <Link
+          to={"/"}
+          as={RouterLink}
+          p={2}
+          display={{ base: "block", md: "none" }}
+          borderRadius={6}
+          _hover={{ bg: "whiteAlpha.200" }}
+          w={10}
+          cursor="pointer"
+        >
+          <InstagramMobileLogo />
+        </Link>
+        <Flex direction={"column"} gap={5} cursor={"pointer"}>
+          {sidebarItems.map((item, index) => (
+            <Tooltip
+              key={index}
+              hasArrow
+              label={item.text}
+              placement="right"
+              ml={1}
+              openDelay={400}
+              display={{ base: "block", md: "none" }}
+            >
+              <Link
+                display={"flex"}
+                to={item.link || null}
+                as={RouterLink}
+                alignContent={"center"}
+                gap={4}
+                _hover={{ bg: "whiteAlpha.400" }}
+                borderRadius={6}
+                p={2}
+                w={{ base: 10, md: "full" }}
+                justifyContent={{ base: "center", md: "flex-start" }}
+              >
+                {item.icon}
+                <Box display={{ base: "none", md: "block" }}>{item.text}</Box>
+              </Link>
+            </Tooltip>
+          ))}
+        </Flex>
+      </Flex>
+    </Box>
+  );
 };
 
 export default SideBar;
