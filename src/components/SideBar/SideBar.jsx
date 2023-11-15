bae786c6f00eee2630a8fccfaec6c3fc394fd1cf
dev-from-cbd <veryfatwombat@gmail.com>
2023-11-16 06:51:49 +1030

    Update

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -69,35 +69,31 @@ const SideBar = () => {
         >
           <InstagramMobileLogo />
         </Link>
-        <Flex direction={"column"} gap={5} cursor={"pointer"}>
-          {sidebarItems.map((item, index) => (
-            <Tooltip
-              key={index}
-              hasArrow
-              label={item.text}
-              placement="right"
-              ml={1}
-              openDelay={400}
-              display={{ base: "block", md: "none" }}
-            >
-              <Link
-                display={"flex"}
-                to={item.link || null}
-                as={RouterLink}
-                alignContent={"center"}
-                gap={4}
-                _hover={{ bg: "whiteAlpha.400" }}
-                borderRadius={6}
-                p={2}
-                w={{ base: 10, md: "full" }}
-                justifyContent={{ base: "center", md: "flex-start" }}
-              >
-                {item.icon}
-                <Box display={{ base: "none", md: "block" }}>{item.text}</Box>
-              </Link>
-            </Tooltip>
-          ))}
-        </Flex>
+
+        <Tooltip
+          hasArrow
+          label={item.text}
+          placement="right"
+          ml={1}
+          openDelay={400}
+          display={{ base: "block", md: "none" }}
+        >
+          <Link
+            display={"flex"}
+            to={item.link || null}
+            as={RouterLink}
+            alignContent={"center"}
+            gap={4}
+            _hover={{ bg: "whiteAlpha.400" }}
+            borderRadius={6}
+            p={2}
+            w={{ base: 10, md: "full" }}
+            justifyContent={{ base: "center", md: "flex-start" }}
+          >
+            {item.icon}
+            <Box display={{ base: "none", md: "block" }}>{item.text}</Box>
+          </Link>
+        </Tooltip>
       </Flex>
     </Box>
   );
