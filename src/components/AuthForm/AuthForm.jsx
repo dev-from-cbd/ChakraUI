66b3f39f4bf70a4fda1e947293251c435f84fc93
dev-from-cbd <veryfatwombat@gmail.com>
2023-11-21 22:05:30 +1030

    Update

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -6,11 +6,13 @@ import {
   Input,
   Text,
   VStack,
-} from "@chakra-ui/react";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+} from "@chakra-ui/react"; // Importing Chakra UI components
+import { useState } from "react"; // Importing React's useState hook
+import { useNavigate } from "react-router-dom"; // Importing React Router's useNavigate hook
 
+// Defining the AuthForm functional component
 const AuthForm = () => {
+  // State variables for managing login/signup mode, navigation, and form inputs
   const [isLogin, setIsLogin] = useState(true);
   const navigate = useNavigate();
   const [inputs, setInputs] = useState({
@@ -19,23 +21,31 @@ const AuthForm = () => {
     confirmPassword: "",
   });
 
+  // Function to handle authentication when the form is submitted
   const handleAuth = () => {
     if (!inputs.email || !inputs.password) {
       alert("Please fill all the fields");
       return;
     }
+    // Additional authentication logic can be added here
   };
 
+  // JSX structure for the AuthForm component
   return (
     <>
+      {/* Container for the entire form */}
       <Box border={"1px solid gray"} borderRadius={6} padding={2}>
+        {/* Vertical stack for organizing form elements */}
         <VStack spacing={4}>
+          {/* Project logo image */}
           <Image
             src="/fake.jpeg"
             h={39.99}
             cursor={"pointer"}
             alt="Logo of Project"
           />
+
+          {/* Email input field */}
           <Input
             placeholder="Email"
             fontSize={15}
@@ -43,17 +53,20 @@ const AuthForm = () => {
             value={inputs.email}
             onChange={(e) => setInputs({ ...inputs, email: e.target.value })}
           />
+
+          {/* Password input field */}
           <Input
-            placeholder="PassWord"
+            placeholder="Password"
             fontSize={15}
             type="password"
             value={inputs.password}
             onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
           />
 
+          {/* Confirm Password input field, displayed only for sign-up */}
           {!isLogin ? (
             <Input
-              placeholder="Confirm PassWord"
+              placeholder="Confirm Password"
               fontSize={15}
               type="password"
               value={inputs.confirmPassword}
@@ -63,6 +76,7 @@ const AuthForm = () => {
             />
           ) : null}
 
+          {/* Button for submitting the form */}
           <Button
             w="full"
             colorScheme="green"
@@ -73,7 +87,7 @@ const AuthForm = () => {
             {isLogin ? "Log in" : "Sign Up"}
           </Button>
 
-          {/* OR */}
+          {/* OR Separator */}
           <Flex
             alignItems={"center"}
             justifyContent={"center"}
@@ -88,6 +102,7 @@ const AuthForm = () => {
             <Box flex={2} h={"1px"} bg={"blue.500"} />
           </Flex>
 
+          {/* Google Auth option */}
           <Flex
             alignItems={"center"}
             justifyContent={"center"}
@@ -105,8 +120,10 @@ const AuthForm = () => {
       <Box border={"2px solid red"} borderRadius={"6"} padding={3}>
         <Flex alignItems={"center"} justifyContent={"center"} w={"246px"}>
           <Box mx={1} fontSize={15}>
+            {/* Dynamic text based on login/signup mode */}
             {isLogin ? "Don't have an account?" : "Already have an account?"}
           </Box>
+          {/* Toggle button for switching login/signup mode */}
           <Box
             mx={1}
             fontSize={15}
