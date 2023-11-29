49ddbe91ff6a4ef9d3c74dc590a94b051f67d734
dev-from-cbd <veryfatwombat@gmail.com>
2023-11-30 06:45:40 +1030

    Update

--- src/components/FeedPosts/PostFooter.jsx
+++ src/components/FeedPosts/PostFooter.jsx
@@ -1,9 +1,16 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Box } from "@chakra-ui/react";
+import { useState } from "react";
+import NotificationsLogo, { UnlikeLogo } from "../../assets/constants";
 
 const PostFooter = () => {
+  const [liked, setLiked] = useState(false);
+  const [likes, setLikes] = useState(1000);
+  //const [comment, setComment] = useState(1000);
   return (
     <>
-      <Flex>Post Footer</Flex>
+      <Flex alignItems={"center"} gap={4} w="full" pt={0} mb={2} mt={"auto"}>
+        <Box>{!liked ? <NotificationsLogo /> : UnlikeLogo}</Box>
+      </Flex>
     </>
   );
 };
