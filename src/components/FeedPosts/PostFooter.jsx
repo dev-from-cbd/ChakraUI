7f9d394ccce12a43ce79fcdb9593bd749a683a3c
dev-from-cbd <veryfatwombat@gmail.com>
2023-12-01 06:36:22 +1030

    Update

--- src/components/FeedPosts/PostFooter.jsx
+++ src/components/FeedPosts/PostFooter.jsx
@@ -1,16 +1,34 @@
 import { Flex, Box } from "@chakra-ui/react";
 import { useState } from "react";
-import NotificationsLogo, { UnlikeLogo } from "../../assets/constants";
+import NotificationsLogo from "../../assets/constants";
+import UnlikeLogo from "../../assets/constants";
+import CommentLogo from "../../assets/constants";
 
 const PostFooter = () => {
   const [liked, setLiked] = useState(false);
   const [likes, setLikes] = useState(1000);
-  //const [comment, setComment] = useState(1000);
+  const handleLike = () => {
+    if (liked) {
+      setLiked(false);
+      setLikes(likes - 1);
+    } else {
+      setLiked(true);
+      setLikes(likes + 1);
+    }
+  };
   return (
     <>
       <Flex alignItems={"center"} gap={4} w="full" pt={0} mb={2} mt={"auto"}>
-        <Box>{!liked ? <NotificationsLogo /> : UnlikeLogo}</Box>
+        <Box onClick={handleLike} cursor={"pointer"} fontSize={18}>
+          {!liked ? <NotificationsLogo /> : <UnlikeLogo />}
+        </Box>
+        <Box cursor={"pointer"} fontSize={18}>
+          <CommentLogo />
+        </Box>
       </Flex>
+      <Text>
+        {likes} {likes === 1 ? "like" : "likes"}
+      </Text>
     </>
   );
 };
